901b6dfc7c272099e85300078b77d463fe9cede9
Jonathan Langevin <codingsloth@pm.me>
2026-06-30 05:42:50 -0400

    chore: update wfctl to v0.83.3 (#55)
    Co-authored-by: goreleaserbot <bot@goreleaser.com>

--- Formula/wfctl.rb
+++ Formula/wfctl.rb
@@ -1,28 +1,28 @@
 class Wfctl < Formula
   desc "CLI for the workflow orchestration engine — validate, inspect, deploy, and manage configs"
   homepage "https://github.com/GoCodeAlone/workflow"
-  version "0.83.2"
+  version "0.83.3"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
       url "https://github.com/GoCodeAlone/workflow/releases/download/v#{version}/wfctl-darwin-amd64"
-      sha256 "4e110aabce64fa5920fcce81322b3303c771a270f8a7853c3bd2b5c7f1d936fe"
+      sha256 "096b0c865686f49add9a8be728b5ce3995f68aac0781e3777e5d2d0dada368f3"
     end
     on_arm do
       url "https://github.com/GoCodeAlone/workflow/releases/download/v#{version}/wfctl-darwin-arm64"
-      sha256 "c1bf4a75028a7e370d4ec8d11498ee5f7eaee564b429f98a2553fffc3d398ac5"
+      sha256 "6960933fe4c129e3efe4a73d15fb299d691497c277467f1e77e2130fe3be0ae5"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/GoCodeAlone/workflow/releases/download/v#{version}/wfctl-linux-amd64"
-      sha256 "21a34cb3a0f8967439e3f1f84f24cded8bf3c37efef94228e3c98ca2c5e27599"
+      sha256 "b12aee41e84039239e51e74266022a13febf6ece1a49bcbfb28a110639265b29"
     end
     on_arm do
       url "https://github.com/GoCodeAlone/workflow/releases/download/v#{version}/wfctl-linux-arm64"
-      sha256 "a1840fbe096c91e386b344b342837b181e731d9471686dd5ebb2da7ddc4736e4"
+      sha256 "4880186a58c2c4c46180fafcec6eb00af43fd1d993a3d98af80dec77a7758796"
     end
   end
 
