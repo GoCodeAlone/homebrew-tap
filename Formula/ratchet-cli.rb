8ed8bd471420aa96a99f2e643444e45e74500bfd
Jonathan Langevin <codingsloth@pm.me>
2026-07-03 07:12:21 -0400

    brew: update ratchet-cli formula to 0.25.0 (#60)

--- Formula/ratchet-cli.rb
+++ Formula/ratchet-cli.rb
@@ -1,28 +1,28 @@
 class RatchetCli < Formula
   desc "Interactive AI agent CLI"
   homepage "https://github.com/GoCodeAlone/ratchet-cli"
-  version "0.2.0"
+  version "0.25.0"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
       url "https://github.com/GoCodeAlone/ratchet-cli/releases/download/v#{version}/ratchet_darwin_amd64.tar.gz"
-      sha256 "4b04f09466d06e7fe608a73d4fff92ba0b7ccf5d1b568e1fbed2fe002bd0169f"
+      sha256 "46984ecc4c806d11b557c79dfb6bb0609db8d18fcbfb0d6f2448fc856f7b4fff"
     end
     on_arm do
       url "https://github.com/GoCodeAlone/ratchet-cli/releases/download/v#{version}/ratchet_darwin_arm64.tar.gz"
-      sha256 "f71274c6594d0a91f84e5125eb3adff8a409a03c7a7385d667200b10f70a0690"
+      sha256 "8c9ad45aebc6a8a34c257faa640a5d16ee1160b4964653d5ed7b9f38927175ff"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/GoCodeAlone/ratchet-cli/releases/download/v#{version}/ratchet_linux_amd64.tar.gz"
-      sha256 "72f6a7055cfe4884d1197f6ee9f99bc056d6da07ef582190aa997dc4a2dfa94b"
+      sha256 "b524f31fa64ecbe3157deaf136d0239afd4b26716b3f2b37ba30dd6e56d10cea"
     end
     on_arm do
       url "https://github.com/GoCodeAlone/ratchet-cli/releases/download/v#{version}/ratchet_linux_arm64.tar.gz"
-      sha256 "8934981cf4489a9bebef972cd1d70c6547ea455560c07d59e672dd012e4c0c42"
+      sha256 "1feea887c07c0e1f264924d0c4f668237d921e4dc5a995d0afd15ea79e016249"
     end
   end
 
